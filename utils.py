2167145d4227da6f6e1d806c9c291e64c5abaaf2
Bennet Rupnow <bennet.rupnow@gym-meiendorf.de>
2021-06-12 11:21:54 +0200

    Add timer class

--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
+from datetime import datetime, timedelta
+
+
 def clamp(num: int, min_value: int, max_value: int):
     return max(min(num, max_value), min_value)
 
 
 def is_in_range(num, min, max):
     return min <= num <= max
+
+
+class Timer:
+    def __init__(self, countdown_time):
+        self.countdown_time = timedelta(seconds=countdown_time)
+        self.startTime = datetime.now()
+        self.done = False
+
+    def update(self):
+        self.done = self.startTime + self.countdown_time <= datetime.now()
