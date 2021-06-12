b5f4136addb1c99b254aafcf798067a3d72f88e1
Bennet Rupnow <bennet.rupnow@gym-meiendorf.de>
2021-06-12 02:14:44 +0200

    Movable wagon

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import pygame
 import sys
+import gameobjects
 
 
 def setup():
     global clock
     global running
     global screen
+    global wagon
+    global wagon_group
     pygame.init()
     screen = pygame.display.set_mode([600, 600])
     pygame.display.set_caption("Game with a Player")
     clock = pygame.time.Clock()
     running = True
 
+    # Wagon
+    wagon = gameobjects.Wagon(None, pygame.Vector2(200, 0))
+    wagon.set_target(pygame.Vector2(200, 200))
+    wagon_group = pygame.sprite.Group()
+    wagon_group.add(wagon)
+
 
 def update():
-    print("")
+    wagon.update()
 
 
 def draw():
     screen.fill((0, 0, 0))
+    wagon_group.draw(screen)
     pygame.display.update()
 
 
