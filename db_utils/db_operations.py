1e6ae325d84dc91993f8ceb8f9d48997f1668fd5
alineary <alinadvries@googlemail.com>
2021-06-13 10:56:34 +0200

    Now fetches results from SELECT

--- db_utils/db_operations.py
+++ db_utils/db_operations.py
@@ -9,7 +9,8 @@ print(conn)
 def get_top_five():
     query = "SELECT * FROM dr4072gl6cmha.public.highscores ORDER BY score DESC LIMIT 5"
     cursor = conn.cursor()
-    result = cursor.execute(query)
+    cursor.execute(query)
+    result = cursor.fetchall()
     cursor.close()
     return result
 
@@ -18,10 +19,6 @@ def add_new_score(name, score):
     query = "INSERT INTO dr4072gl6cmha.public.highscores(name, score) VALUES (%s, %s)"
     data = (name, score)
     cursor = conn.cursor()
-    result = cursor.execute(query, data)
+    cursor.execute(query, data)
     conn.commit()  # <- We MUST commit to reflect the inserted data
     cursor.close()
-    return result
-
-
-print(add_new_score('Peter', 69))
